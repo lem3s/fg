63a462eb9ebd8a5ae18dd6cb921716daebee59f9
montebello <montebellovictor@gmail.com>
2025-03-27 14:07:29 -0300

    sample boiletplate of app

---
+++ common/watcher/watcher.go
@@ -0,0 +1,40 @@
+package watcher
+
+import (
+	"fmt"
+	"os"
+	"time"
+	"syscall"
+)
+
+type ProcessWatcher struct {
+	Pid int
+}
+
+func NewWatcher(pid int) *ProcessWatcher {
+	return &ProcessWatcher{Pid: pid}
+}
+
+func (w *ProcessWatcher) Watch() error {
+	process, err := os.FindProcess(w.Pid)
+	if err != nil {
+		return fmt.Errorf("process not found: %v", err)
+	}
+
+	for {
+		err := process.Signal(syscall.Signal(0))
+		if err != nil {
+			return fmt.Errorf("process no longer exists: %v", err)
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func (w *ProcessWatcher) IsRunning() bool {
+	process, err := os.FindProcess(w.Pid)
+	if err != nil {
+		return false
+	}
+	err = process.Signal(syscall.Signal(0))
+	return err == nil
+}
