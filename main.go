1153abde3044c6dae5720bb2602ba634ba139777
Luquetti <gustavo.luquetti@hotmail.com>
2025-04-02 17:15:20 -0300

    commit da service list

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-<<<<<<< HEAD
 import (
 	"fmt"
 	"module/common/services"
@@ -9,10 +8,4 @@ func main() {
 	fmt.Println("começa aqui")
 	services.ListVersions()
 	
-=======
-import "fmt"
-
-func main() {
-	fmt.Println("começa aqui")
->>>>>>> dc765d8b94c0f6fcb3cd03cec1c223194d774d9d
 }
