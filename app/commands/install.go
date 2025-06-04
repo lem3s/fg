e7a9b1e4a1acc32b2244f0ca00a3c41bf6f69e90
lem3s <joaovictorlemesfaria@gmail.com>
2025-06-03 22:34:34 -0300

    feat: implement JDK installation command
    - Add InstallCmd for managing JDK installation
    - Add archiever dependency to extract packages os agnostic
    - Download and extract JDK based on OS

--- app/commands/install.go
+++ app/commands/install.go
@@ -1,21 +1,151 @@
 package commands
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/lem3s/fg/app/cmd"
+	"github.com/mholt/archiver/v3"
 )
 
 type InstallCmd struct {
 	Ctx *cmd.AppContext
 }
 
-func (h *InstallCmd) Run(args []string) error {
-	h.Ctx.Config.Set("jar", "teste")
-	h.Ctx.Config.WriteConfig()
-	return nil
-}
-
 func init() {
 	cmd.Register("install", func(ctx *cmd.AppContext) cmd.Command {
 		return &InstallCmd{Ctx: ctx}
 	})
+}
+
+func (h *InstallCmd) Run(args []string) error {
+	// TODO[Lemes] : alterar quando tiverem as flags
+	installVersion := "1.2.3"
+
+	versionsData, err := cmd.GetVersionsData()
+	if err != nil {
+		return err
+	}
+
+	versionInfo := getVersion(versionsData, installVersion)
+	if versionInfo == nil {
+		return fmt.Errorf("FG %s not available in repository", installVersion)
+	}
+
+	// Start of JDK install
+	fileExtension := getFileExtension(h.Ctx.OS)
+
+	err = downloadCompressedJDK(
+		versionInfo.JDK.PlatformsCompressedSourceUrl[h.Ctx.OS],
+		filepath.Join(h.Ctx.FgHome, "temp"),
+		"jdk."+fileExtension,
+	)
+	if err != nil {
+		return err
+	}
+
+	if !folderExists(filepath.Join(h.Ctx.FgHome, installVersion, "jdk")) {
+		err = extractCompressedJDK(
+			filepath.Join(h.Ctx.FgHome, "temp", "jdk."+fileExtension),
+			filepath.Join(h.Ctx.FgHome, installVersion, "jdk"),
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	javaExecutablePath, err := searchForJavaExecutable(
+		filepath.Join(h.Ctx.FgHome, installVersion, "jdk"),
+		h.Ctx.OS,
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(javaExecutablePath)
+
+	// End of JDK install
+
+	return nil
+}
+
+func getVersion(versionsData []cmd.Version, installVersion string) *cmd.Version {
+	for _, version := range versionsData {
+		if version.Version == installVersion {
+			return &version
+		}
+	}
+
+	return nil
+}
+
+func getFileExtension(os string) string {
+	if os == "windows" {
+		return "zip"
+	} else {
+		return "tar.gz"
+	}
+}
+
+func downloadCompressedJDK(url, destinationDirectory, fileNameDestination string) error {
+	err := os.MkdirAll(destinationDirectory, 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create directory: %w", err)
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath.Join(destinationDirectory, fileNameDestination))
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func extractCompressedJDK(sourceDirectory, destinationDirectory string) error {
+	return archiver.Unarchive(sourceDirectory, destinationDirectory)
+}
+
+func searchForJavaExecutable(rootDir, system string) ([]string, error) {
+	var targetFile string
+	if system == "windows" {
+		targetFile = "java.exe"
+	} else {
+		targetFile = "java"
+	}
+
+	var matches []string
+
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("Warning: cannot access %s: %v\n", path, err)
+			return filepath.SkipDir
+		}
+
+		if !info.IsDir() && strings.EqualFold(info.Name(), targetFile) {
+			matches = append(matches, path)
+		}
+
+		return nil
+	})
+
+	return matches, err
+}
+
+func folderExists(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
 }
--- go.mod
+++ go.mod
@@ -9,14 +9,19 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
+	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
+	github.com/klauspost/compress v1.11.4 // indirect
+	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/labstack/echo/v4 v4.13.3 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leaanthony/go-ansi-parser v1.6.1 // indirect
@@ -25,7 +30,10 @@ require (
 	github.com/leaanthony/u v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mholt/archiver/v3 v3.5.1 // indirect
+	github.com/nwaples/rardecode v1.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pierrec/lz4/v4 v4.1.2 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
@@ -37,10 +45,12 @@ require (
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tkrajina/go-reflector v0.5.8 // indirect
+	github.com/ulikunitz/xz v0.5.9 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/wailsapp/go-webview2 v1.0.19 // indirect
 	github.com/wailsapp/mimetype v1.4.1 // indirect
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
